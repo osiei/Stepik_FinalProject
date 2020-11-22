2fab5486a771d02025d58fec005261c5399d9b36
osiei <e.tsibizova@yandex.ru>
2020-11-22 21:19:22 +0300

    verify form registration and login

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,3 +8,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page2 = LoginPage(browser, link)
+    page2.open()
+    page2.should_be_login_page()
