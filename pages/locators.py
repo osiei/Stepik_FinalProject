cd622f33b9c4f5f837a4c3e87a2d4f54ceeb8bdc
osiei <e.tsibizova@yandex.ru>
2021-01-18 20:45:56 +0300

    final refactoring for review

--- pages/locators.py
+++ pages/locators.py
@@ -15,19 +15,19 @@ class RegistrationFormLocators():
     EMAIL_TEXT = (By.CSS_SELECTOR, "#id_registration-email")
     PASSWORD_TEXT = (By.CSS_SELECTOR, "#id_registration-password1")
     PASSWORD_TEXT_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
-    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "#register_form > button")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "[name=registration_submit]")
 
 
 class BusketPageLocators():
     BUSKET_BUTTON = (
-    By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
-    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
-    NAME_PRODUCT = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
-    SUM_PRODUCT = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    By.CSS_SELECTOR, ".btn-group > a")
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.product_main > h1")
+    SUM_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.product_main > p.price_color")
     ALERT_NAME_PRODUCT_IN_BUSKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
     ALERT_SUM_PRODUCT_IN_BUSKET = (
         By.CSS_SELECTOR,
-        "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+        ".alert.alert-safe.alert-noicon.alert-info.fade.in >.alertinner > p:nth-child(1) > strong")
     EMPTY_BUSKET = (By.CSS_SELECTOR, "#content_inner > p")
     PRODUCT_IN_BUSKET = (By.CSS_SELECTOR, "#basket_formset > div > div")
 
